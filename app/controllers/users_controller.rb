b8eaea0c44fad6bd587e7861850af43725172a30
ikedaema2018 <ikedaema0817>
2018-07-30 22:11:29 +0900

    登録、更新、削除、一覧、ユーザー詳細

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,60 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index 
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/users'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'ユーザー情報を更新しました'}
+        format.json { render ：show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'ユーザーの削除に成功しました' }
+      format.json { head :no_contact }
+    end
+  end
+
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confimation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "invalid signup infomation" do 
+  #   get new_user_path
+  #   assert_no_difference 'User.count' do
+  #     post users_path, params: { user: { name: "",
+  #       email: "user@invalid",
+  #       password: "foo",
+  #       password_confirmation: "foo"
+  #       }}
+  #   end
+  #   assert_template 'users/new'
+  # end
+
+end
